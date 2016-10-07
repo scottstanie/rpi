5aaeb0a9af9c6d9db86ad0e1639cf86e9bd632a9
scott <scott@cogolabs.com>
2016-10-06 22:39:06 -0400

    posts are working

--- app.py
+++ app.py
@@ -1,12 +1,37 @@
 from __future__ import unicode_literals
 import os
-from flask import Flask, render_template, request, jsonify
-import json, requests
+from flask import Flask, render_template, jsonify, request
+# import json
+# import requests
+
+# import time
+# import atexit
+
+# from Adafruit_MotorHAT import Adafruit_MotorHAT
 
 
 RESULTS_PER_PAGE = 10
 # configuration
 DEBUG = True
+
+# import Robot
+LEFT_TRIM   = 0
+RIGHT_TRIM  = 0
+
+
+# # Create an instance of the robot with the specified trim values.
+# # Not shown are other optional parameters:
+# #  - addr: The I2C address of the motor HAT, default is 0x60.
+# #  - left_id: The ID of the left motor, default is 1.
+# #  - right_id: The ID of the right motor, default is 2.
+# robot = Robot.Robot(left_trim=LEFT_TRIM, right_trim=RIGHT_TRIM)
+# ROBOT_FUNCTIONS = {
+#     'up': robot.forward,
+#     'down': robot.backward,
+#     'left': robot.left,
+#     'right': robot.right,
+#     'stop': robot.stop
+# }
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -15,6 +40,19 @@ app.config.from_object(__name__)
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+
+@app.route('/move', methods=['POST'])
+def move():
+    data = request.get_json()
+    speed = data['speed'].lower()
+    direction = data['direction'].lower()
+    speeds = {'fast': 250, 'slow': 125}
+    speed_num = speeds[speed]
+    print speed_num, direction
+    # ROBOT_FUNCTIONS[direction](speed_num)
+    return jsonify({'response': 'OK'})
+
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
--- static/js/app.js
+++ static/js/app.js
@@ -1,23 +1,31 @@
 $(document).ready(function() {
   console.log('Ready');
+  var $speed = $('.active.speed').children().text()
+
+  $('.speed').on('click tap', function() {
+    $('.speed').removeClass('active');
+    $(this).addClass('active');
+    $speed = $('.active.speed').children().text()
+  })
   $(".arrow").on('mousedown vmousedown touchstart', function() {
     $(this).addClass('active');
-    sendMove($(this).attr('id'));
+    sendMove($(this).attr('id'), $speed);
   })
 
   $(".arrow").on('mouseup vmouseup mouseout touchend', function() {
     $(this).removeClass('active');
-    sendMove('stop');
+    sendMove('stop', $speed);
   })
 
 })
 
-function sendMove(direction) {
-  $.ajax('/update_results', {
-      type: 'GET',
+function sendMove(direction, speed) {
+  $.ajax('/move', {
+      type: 'POST',
       contentType: 'application/json',
       data: JSON.stringify({
-        first_id: first_id
+        direction: direction,
+        speed: speed
       }),
       success: function(data) {
         console.log(data);
